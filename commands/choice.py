db25ac636d3f6ddc81ff6bc671eed0971d1a7a45
DaeHyun Kim <daehyeoni3333@gmail.com>
2024-01-09 17:22:54 +0900

    Add Command: /choice (#30)

---
+++ commands/choice.py
@@ -0,0 +1,14 @@
+import random
+
+from telegram import Update
+from telegram.ext import ContextTypes
+
+
+async def choice(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    choices = context.args
+    if len(choices) < 2:
+        await update.message.reply_text("2개 이상의 선택지를 입력해주세요.")
+        return
+
+    result = random.choice(choices)
+    await update.message.reply_text(result)
--- create_commands.py
+++ create_commands.py
@@ -1,10 +1,10 @@
-from functools import partial
-from typing import Dict
-
+from commands import choice
 from commands.commands import CommandHandler
 from commands.ping import ping
 from commands.stock import currency, stock
 from commands.stock.common import Country, KoreanMarketType
+from functools import partial
+from typing import Dict
 
 commands: Dict[str, Dict] = {
     "ping": {"func": ping, "help": ["/ping: 상태 체크"]},
@@ -53,6 +53,10 @@ commands: Dict[str, Dict] = {
     "jpy": {
         "func": partial(currency.get_currency_data, code=Country.JAPAN),
         "help": ["/jpy: 일본 환율"],
+    },
+    "choice": {
+        "func": choice.choice,
+        "help": ["\n/choice 선택1 선택2 선택3... : 선택지 중 하나를 랜덤으로 골라줍니다."],
     },
 }
 
