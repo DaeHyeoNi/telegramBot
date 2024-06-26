f160a1d3a3bad4584624d748e3018527ea983a8f
daehyeoni <daehyeoni3333@gmail.com>
2024-06-26 12:25:26 +0900

    Fix Lint

--- commands/stock/stock.py
+++ commands/stock/stock.py
@@ -118,13 +118,17 @@ def fetch_usstock_data(ticker: str, flat: bool = False) -> Tuple[str, str]:
 
     _display = data["chart_section"]["default_display"]
 
-    primary_value = _display['primary_value']['value']
+    primary_value = _display["primary_value"]["value"]
     last_trade_price = float(data["chart_section"]["quote"]["last_trade_price"])
     previous_close_price = float(data["chart_section"]["quote"]["previous_close"])
-    secondary_value, secondary_value_desc = _display['secondary_value']['main']['value'], translate_market_desc(_display['secondary_value']['description']['value'])
-    tertiary_value, tertiary_value_desc = _display['tertiary_value']['main']['value'], translate_market_desc(_display['tertiary_value']['description']['value'])
+    secondary_value, secondary_value_desc = _display["secondary_value"]["main"][
+        "value"
+    ], translate_market_desc(_display["secondary_value"]["description"]["value"])
+    tertiary_value, tertiary_value_desc = _display["tertiary_value"]["main"][
+        "value"
+    ], translate_market_desc(_display["tertiary_value"]["description"]["value"])
 
-    last_market_name = '정규장'
+    last_market_name = "정규장"
 
     message = f"{primary_value} ({last_trade_price} <- {previous_close_price})"
     if secondary_value:
